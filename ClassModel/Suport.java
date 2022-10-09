b74a25ff9ecca275b9ada767fdba193d7dbd1b88
Leticia <lcso.silva@campus.fct.unl.pt>
2022-10-09 14:06:21 +0100

    Initial commit

---
+++ ClassModel/Suport.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Suport {
+
+    /**
+     * Default constructor
+     */
+    public Suport() {
+    }
+
+    /**
+     * 
+     */
+    public ERole role;
+
+    /**
+     * 
+     */
+    public float hours;
+
+}
